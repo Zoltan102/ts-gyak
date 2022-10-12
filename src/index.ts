3acacf87ba3be93a345cdf668970d34cea3217b7
Zoli <suveges.zoltan@petrik.hu>
2022-10-12 12:28:30 +0200

    asd

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+let t = [1, 2, 3];
+t.filter(e => e > 2);
+
+
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('szoveg')!.addEventListener('input', (e) => {
+        //  Option #1
         // let elem = document.getElementById('szoveg') as HTMLInputElement;
         // document.body.style.backgroundColor = elem.value;
 
+        //  Option #2
         let elem = e.currentTarget as HTMLInputElement;
         document.body.style.backgroundColor = elem.value;
     });
